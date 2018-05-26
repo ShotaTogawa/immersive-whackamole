557abf3413df9f95859a56e9c0a8cf1d2d94a279
Shota Togawa <s.togawa0112@gmail.com>
2018-05-26 21:20:03 +0900

    completed checkpoint-0

--- checkpoints/checkpoint-0.md
+++ checkpoints/checkpoint-0.md
@@ -2,10 +2,10 @@
 
 **Live Coding!** In this checkpoint, watch a tech mentor live code the following:
 
-- [ ] Create an App parent component
-- [ ] Create the markup for all of the components, irrespective of any data
-- [ ] Add css styles to render the moles sideways
-- [ ] Use the `data` VueJS component property to set some custom data
-- [ ] Use the `{{ }}` double-bracket value interpolation in your template to show the custom data
-- [ ] Use the `method` VueJS component property to add a custom function
-- [ ] Show v-on:click for event handling
+- [x] Create an App parent component
+- [x] Create the markup for all of the components, irrespective of any data
+- [x] Add css styles to render the moles sideways
+- [x] Use the `data` VueJS component property to set some custom data
+- [x] Use the `{{ }}` double-bracket value interpolation in your template to show the custom data
+- [x] Use the `method` VueJS component property to add a custom function
+- [x] Show v-on:click for event handling
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,51 @@
 <template>
+<div class="whackamole">
+    <h1 class="logo">
+      Whackamole
+    </h1>
+    <button
+    v-on:click='startGame'>
+      Start Game
+    </button>
+    <div class="counters-container">
+      <div class="counter">
+        <h1>Score:</h1>
+        <h2>{{ score }}</h2>
+      </div>
+      <div class="counter">
+        <h1>High Score:</h1>
+        <h2>{{ highScore }}</h2>
+      </div>
+      <div class="counter">
+        <h1>Timer:</h1>
+        <h2>{{ timer }}</h2>
+      </div>
+    </div>
+    <div class="moles-container">
+      <div class="mole">{{ moles[0]}}</div>
+      <div class="mole">{{ moles[1]}}</div>
+      <div class="mole">{{ moles[2]}}</div>
+      <div class="mole">{{ moles[3]}}</div>
+    </div>
+  </div>
   
 </template>
 
 <script>
 export default {
+    data: ()=>{
+        return{
+            score: 10,
+            highScore: 20,
+            timer: 10,
+            moles: [false,false,false,false]
+        }
+    },
+    methods:{
+        startGame: function(){
+            console.log('start');
+        }
+    }
   
 }
 </script>
